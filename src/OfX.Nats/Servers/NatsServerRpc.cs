8244ca00b811a0b37ca7a33fea9195cceaf35af8
quyvu <quy.vu.0101@gmail.com>
2025-02-10 17:33:45 +0700

    Update INatsServerRpc

--- src/OfX.Nats/Servers/NatsServerRpc.cs
+++ src/OfX.Nats/Servers/NatsServerRpc.cs
@@ -13,26 +13,23 @@ internal sealed class NatsServerRpc<TModel, TAttribute>(IServiceProvider service
     : INatsServerRpc<TModel, TAttribute>
     where TAttribute : OfXAttribute where TModel : class
 {
-    public void StartAsync()
+    public async Task StartAsync()
     {
         var natsClient = serviceProvider.GetRequiredService<NatsClientWrapper>();
-        Task.Factory.StartNew(async () =>
+        var natsScribeAsync = natsClient.NatsClient
+            .SubscribeAsync<MessageDeserializable>(typeof(TAttribute).GetNatsSubject());
+        await foreach (var message in natsScribeAsync)
         {
-            var natsScribeAsync = natsClient.NatsClient
-                .SubscribeAsync<MessageDeserializable>(typeof(TAttribute).GetNatsSubject());
-            await foreach (var message in natsScribeAsync)
-            {
-                if (message.Data is null) continue;
-                using var serviceScope = serviceProvider.CreateScope();
-                var pipeline = serviceScope.ServiceProvider
-                    .GetRequiredService<ReceivedPipelinesOrchestrator<TModel, TAttribute>>();
-                var headers = message.Headers?
-                    .ToDictionary(a => a.Key, b => b.Value.ToString()) ?? [];
-                var requestOf = new RequestOf<TAttribute>(message.Data.SelectorIds, message.Data.Expression);
-                var requestContext = new RequestContextImpl<TAttribute>(requestOf, headers, CancellationToken.None);
-                var response = await pipeline.ExecuteAsync(requestContext);
-                await natsClient.NatsClient.PublishAsync(message.ReplyTo!, response);
-            }
-        });
+            if (message.Data is null) continue;
+            using var serviceScope = serviceProvider.CreateScope();
+            var pipeline = serviceScope.ServiceProvider
+                .GetRequiredService<ReceivedPipelinesOrchestrator<TModel, TAttribute>>();
+            var headers = message.Headers?
+                .ToDictionary(a => a.Key, b => b.Value.ToString()) ?? [];
+            var requestOf = new RequestOf<TAttribute>(message.Data.SelectorIds, message.Data.Expression);
+            var requestContext = new RequestContextImpl<TAttribute>(requestOf, headers, CancellationToken.None);
+            var response = await pipeline.ExecuteAsync(requestContext);
+            await natsClient.NatsClient.PublishAsync(message.ReplyTo!, response);
+        }
     }
 }
