8ef5c8d1ae886eca4fa44574cf8802fc38376cd7
quyvu <quy.vu.0101@gmail.com>
2025-05-25 22:14:36 +0700

    Update Delegate.DynamicInvoke to Func<object, object> to reduce performance overhead on the core function `MapDataAsync`

--- src/OfX/ApplicationModels/MappableModels.cs
+++ src/OfX/ApplicationModels/MappableModels.cs
@@ -7,22 +7,22 @@ internal sealed record MappableDataProperty(
     PropertyInfo PropertyInfo,
     object Model,
     IOfXAttributeCore Attribute,
-    Delegate Func,
+    Func<object, object> Func,
     string Expression,
     int Order);
 
 internal sealed record MappableDataPropertyCache(
     IOfXAttributeCore Attribute,
-    Delegate Func,
+    Func<object, object> Func,
     string Expression,
     int Order);
 
 
 internal sealed record MappableTypeData(
     Type OfXAttributeType,
-    IEnumerable<PropertyCalledLater> PropertyCalledLaters,
+    IEnumerable<RuntimePropertyCalling> PropertyCalledLaters,
     IEnumerable<string> Expressions,
     int Order);
 
 
-internal sealed record PropertyCalledLater(object Model, Delegate Func);
+internal sealed record RuntimePropertyCalling(object Model, Func<object, object> Func);
