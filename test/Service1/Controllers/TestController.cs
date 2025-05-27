3b1583dee7519489099fd8e4ce0c016bb236ba3c
quyvu <quy.vu.0101@gmail.com>
2025-05-27 21:10:41 +0700

    remove API TestConcurrents!

--- test/Service1/Controllers/TestController.cs
+++ test/Service1/Controllers/TestController.cs
@@ -42,22 +42,6 @@ public sealed class TestController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> FetchConcurrents([FromServices] IDataMappableService dataMappableService)
-    {
-        var result = dataMappableService
-            .FetchDataAsync<CountryOfAttribute>(new DataFetchQuery(["abc"], [null]));
-        var newFunc = async () =>
-        {
-            await Task.Delay(1000);
-            return await dataMappableService
-                .FetchDataAsync<CountryOfAttribute>(new DataFetchQuery(["xyz"], [null]));
-        };
-        var result2 = newFunc.Invoke();
-        await Task.WhenAll(result, result2);
-        return Ok(new { Res1 = result.Result, Res2 = result2.Result });
-    }
-
-    [HttpGet]
     public async Task<IActionResult> NativeDbContextTest([FromServices] OtherService1Context context)
     {
         IEnumerable<string> ids = ["1", "2", "3"];
