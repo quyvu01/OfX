e0afd19e191361362f22d06b5813e432b4662458
quyvu <quy.vu.0101@gmail.com>
2025-07-02 09:27:18 +0700

    add nats options to OfX-Nats => Enhance security when connectting to Nats

--- src/OfX.Nats/ApplicationModels/NatsClientSetting.cs
+++ src/OfX.Nats/ApplicationModels/NatsClientSetting.cs
@@ -1,3 +1,4 @@
+using NATS.Client.Core;
 using OfX.Nats.Statics;
 
 namespace OfX.Nats.ApplicationModels;
@@ -6,4 +7,10 @@ public sealed class NatsClientSetting
 {
     public void Url(string url) => NatsStatics.NatsUrl = url;
     public void TopicPrefix(string topicPrefix) => NatsStatics.NatsTopicPrefix = topicPrefix;
+    public void NatsOpts(Action<NatsOpts>  options)
+    {
+        var opts = new NatsOpts();
+        options.Invoke(opts);
+        NatsStatics.NatsOpts = opts;
+    }
 }
