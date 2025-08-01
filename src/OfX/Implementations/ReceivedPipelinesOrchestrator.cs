44aee35971fcb8ca22407f2ceecbeefcb6392741
quyvu <quy.vu.0101@gmail.com>
2025-08-01 23:43:51 +0700

    add comment to make clarifying

--- src/OfX/Implementations/ReceivedPipelinesOrchestrator.cs
+++ src/OfX/Implementations/ReceivedPipelinesOrchestrator.cs
@@ -18,7 +18,7 @@ public class ReceivedPipelinesOrchestrator<TModel, TAttribute>(
         var handler = handlers.FirstOrDefault(x => x is not DefaultQueryOfHandler);
         if (handler is null) throw new OfXException.CannotFindHandlerForOfAttribute(typeof(TAttribute));
 
-        // Deserialize expressions from Expression
+        // Deserialize expressions from Expression, we handle the custom expressions and original expression as well
         var expressions = JsonSerializer.Deserialize<List<string>>(requestContext.Query.Expression);
         var customExpressions = customExpressionHandlers
             .Select(a => a.CustomExpression());
@@ -32,15 +32,14 @@ public class ReceivedPipelinesOrchestrator<TModel, TAttribute>(
         var resultTask = behaviors.Reverse()
             .Aggregate(() => handler.GetDataAsync(newRequestContext),
                 (acc, pipeline) => () => pipeline.HandleAsync(newRequestContext, acc)).Invoke();
-
-        var customResults = newExpressions.Count != expressions.Count
-            ? customExpressionHandlers
-                .Select(a => (Expression: a.CustomExpression(), ResultTask: a.HandleAsync(
-                    new RequestContextImpl<TAttribute>(requestContext.Query with { Expression = a.CustomExpression() },
-                        requestContext.Headers, requestContext.CancellationToken)))).ToList()
-            : [];
-
-        if (customResults.Count == 0) return await resultTask;
+        
+        if (newExpressions.Count == expressions.Count) return await resultTask;
+        
+        // Handle getting data for custom expressions
+        var customResults = customExpressionHandlers
+            .Select(a => (Expression: a.CustomExpression(), ResultTask: a.HandleAsync(
+                new RequestContextImpl<TAttribute>(requestContext.Query with { Expression = a.CustomExpression() },
+                    requestContext.Headers, requestContext.CancellationToken)))).ToList();
 
         await Task.WhenAll([resultTask, ..customResults.Select(a => a.ResultTask)]);
         var result = await resultTask;
@@ -49,7 +48,8 @@ public class ReceivedPipelinesOrchestrator<TModel, TAttribute>(
             customResults.Select(a => a.ResultTask.Result.Items.Select(k => (k.Id, k.Value, a.Expression)))
                 .SelectMany(a => a)
                 .GroupBy(x => x.Id);
-
+        
+        // Merge custom results with original results
         result.Items.ForEach(it =>
         {
             var customResult = customResultsMerged.FirstOrDefault(a => a.Key == it.Id);
