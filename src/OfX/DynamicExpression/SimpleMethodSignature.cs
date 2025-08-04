84f8e120745b54e1a0509bfeb8c9d820612fd9dd
quyvu <quy.vu.0101@gmail.com>
2025-08-04 14:49:04 +0700

    Update SimpleMethodSignature

--- src/OfX/DynamicExpression/SimpleMethodSignature.cs
+++ src/OfX/DynamicExpression/SimpleMethodSignature.cs
@@ -8,62 +8,47 @@ namespace OfX.DynamicExpression;
 /// </summary>
 internal class SimpleMethodSignature : MethodBase
 {
-	private class SimpleParameterInfo : ParameterInfo
-	{
-		public SimpleParameterInfo(Type parameterType)
-		{
-			ClassImpl = parameterType;
-			DefaultValueImpl = null;
-		}
+    private class SimpleParameterInfo : ParameterInfo
+    {
+        public SimpleParameterInfo(Type parameterType)
+        {
+            ClassImpl = parameterType;
+            DefaultValueImpl = null;
+        }
 
-		public override bool HasDefaultValue => false;
-	}
+        public override bool HasDefaultValue => false;
+    }
 
-	public override MethodAttributes Attributes { get; } = MethodAttributes.Public;
-	public override MemberTypes MemberType { get; } = MemberTypes.Method;
+    public override MethodAttributes Attributes { get; } = MethodAttributes.Public;
+    public override MemberTypes MemberType { get; } = MemberTypes.Method;
 
-	private readonly ParameterInfo[] _parameterInfos;
-	public SimpleMethodSignature(params Type[] parameterTypes)
-	{
-		_parameterInfos = new ParameterInfo[parameterTypes.Length];
-		for (var i = 0; i < parameterTypes.Length; i++)
-		{
-			_parameterInfos[i] = new SimpleParameterInfo(parameterTypes[i]);
-		}
-	}
+    private readonly ParameterInfo[] _parameterInfos;
 
-	public override ParameterInfo[] GetParameters()
-	{
-		return _parameterInfos;
-	}
+    public SimpleMethodSignature(params Type[] parameterTypes)
+    {
+        _parameterInfos = new ParameterInfo[parameterTypes.Length];
+        for (var i = 0; i < parameterTypes.Length; i++)
+        {
+            _parameterInfos[i] = new SimpleParameterInfo(parameterTypes[i]);
+        }
+    }
 
-	public override RuntimeMethodHandle MethodHandle => throw new NotImplementedException();
-	public override string Name => throw new NotImplementedException();
-	public override Type DeclaringType => throw new NotImplementedException();
-	public override Type ReflectedType => throw new NotImplementedException();
+    public override ParameterInfo[] GetParameters() => _parameterInfos;
 
-	public override object[] GetCustomAttributes(bool inherit)
-	{
-		throw new NotImplementedException();
-	}
+    public override RuntimeMethodHandle MethodHandle => throw new NotImplementedException();
+    public override string Name => throw new NotImplementedException();
+    public override Type DeclaringType => throw new NotImplementedException();
+    public override Type ReflectedType => throw new NotImplementedException();
 
-	public override object[] GetCustomAttributes(Type attributeType, bool inherit)
-	{
-		throw new NotImplementedException();
-	}
+    public override object[] GetCustomAttributes(bool inherit) => throw new NotImplementedException();
 
-	public override MethodImplAttributes GetMethodImplementationFlags()
-	{
-		throw new NotImplementedException();
-	}
+    public override object[] GetCustomAttributes(Type attributeType, bool inherit) =>
+        throw new NotImplementedException();
 
-	public override object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture)
-	{
-		throw new NotImplementedException();
-	}
+    public override MethodImplAttributes GetMethodImplementationFlags() => throw new NotImplementedException();
 
-	public override bool IsDefined(Type attributeType, bool inherit)
-	{
-		throw new NotImplementedException();
-	}
+    public override object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters,
+        CultureInfo culture) => throw new NotImplementedException();
+
+    public override bool IsDefined(Type attributeType, bool inherit) => throw new NotImplementedException();
 }
