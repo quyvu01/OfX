5823905247d3cd81ef6f226ce433c89aba657509
quyvu <quy.vu.0101@gmail.com>
2025-10-20 22:32:47 +0700

    Update ServiceTest

--- test/Service1/Program.cs
+++ test/Service1/Program.cs
@@ -42,7 +42,6 @@ builder.Services.AddOfX(cfg =>
             c.AddGrpcHosts("http://localhost:5001", "http://localhost:5002", "http://localhost:5003"));
         cfg.SetRetryPolicy(3, retryAttempt => retryAttempt * TimeSpan.FromSeconds(2),
             (e, ts) => Console.WriteLine($"Error: {e.Message}"));
-        cfg.AddAzureServiceBus(c => c.Host(""));
     })
     .AddOfXEFCore(cfg => cfg.AddDbContexts(typeof(Service1Context), typeof(OtherService1Context)))
     .AddMongoDb(cfg => cfg.AddCollection(memberSocialCollection))
