00a8875c9e3b387e8107bcf11a54dd763da0f5ff
quyvu <quy.vu.0101@gmail.com>
2025-10-20 22:31:09 +0700

    Add ServiceBusClientOptions as Host Config for AzureServiceBus Client

--- src/OfX.Azure.ServiceBus/ApplicationModels/AzureServiceBusClientSetting.cs
+++ src/OfX.Azure.ServiceBus/ApplicationModels/AzureServiceBusClientSetting.cs
@@ -1,3 +1,4 @@
+using Azure.Messaging.ServiceBus;
 using OfX.Azure.ServiceBus.Statics;
 
 namespace OfX.Azure.ServiceBus.ApplicationModels;
@@ -5,8 +6,17 @@ namespace OfX.Azure.ServiceBus.ApplicationModels;
 public sealed class AzureServiceBusClientSetting
 {
     public string ConnectionString { get; private set; }
+    public ServiceBusClientOptions ServiceBusClientOptions { get; private set; }
 
-    public void Host(string connectionString) => ConnectionString = connectionString;
+    public void Host(string connectionString, Action<ServiceBusClientOptions> serviceBusClientOptions = null)
+    {
+        ConnectionString = connectionString;
+        if (serviceBusClientOptions == null) return;
+        var options = new ServiceBusClientOptions();
+        serviceBusClientOptions.Invoke(options);
+        ServiceBusClientOptions = options;
+    }
+
     public void TopicPrefix(string topicPrefix) => AzureServiceBusStatic.TopicPrefix = topicPrefix;
 
     public void MaxConcurrentSessions(int maxConcurrentSessions)
